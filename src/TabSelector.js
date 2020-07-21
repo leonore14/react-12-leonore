41eafda9220e1f7e7b59b9f21c2196d8498d4fad
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-07-21 10:44:11 +0200

    quest

---
+++ src/TabSelector.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class TabSelector extends Component {
+  
+
+  render() {
+    const { activeId } = this.props;
+    return (
+      <div className="TabSelector">
+        <button
+          id="home"
+          onClick={this.props.handleChangeTab}
+          className={activeId === 'home' ? 'active' : ''}
+        >
+          Home
+        </button>
+        <button
+          id="about"
+          onClick={this.props.handleChangeTab}
+          className={activeId === 'about' ? 'active' : ''}
+        >
+          About
+        </button>
+        <button
+          id="contact"
+          onClick={this.props.handleChangeTab}
+          className={activeId === 'contact' ? 'active' : ''}
+        >
+          Contact
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TabSelector;
